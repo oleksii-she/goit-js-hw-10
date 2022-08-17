3abf5788a32ed297f8f595ecf1c6ad1a92335903
Oleksii Shevchenko <vazerius@gmail.com>
2022-08-17 16:33:02 +0300

    +

--- src/index.js
+++ src/index.js
@@ -5,17 +5,18 @@ const debounce = require('lodash.debounce');
 const search = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 
-const countryListItem = document.createElement('li');
-countryList.append(countryListItem);
-
 function handlerSearchCountri(e) {
   const nameSearc = e.target.value.trim();
   fetchCountries(nameSearc).then(data => creatMarkup(data));
 }
+
 function creatMarkup(obj) {
-  const countri = obj[0].name.official;
-  countryListItem.textContent = countri;
-  console.log(countri);
+  console.log(obj);
+  const markup = obj.map(el => {
+    return `<li><img src="${el.flags.png}" alt="flags" width="35"><span class="country-span">${el.name.official}</span></li>`;
+  });
+  countryList.innerHTML = markup;
+  console.log(markup);
 }
 
 //   const coutri = obj.map(el => el.name.official);
